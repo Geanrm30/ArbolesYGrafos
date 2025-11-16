4bc30d0f59df05a63af8118042daf66f08b78a9f
Geanrm30 <garodriguezm@uamv.edu.ni>
2025-11-16 17:54:01 -0600

    Implementar algoritmos del grafo

--- ArbolesYGrafos/Graph.cs
+++ ArbolesYGrafos/Graph.cs
@@ -44,5 +44,85 @@ namespace ArbolesYGrafos
                 return new List<(string nodo, int distancia)>();
             return Ady[nodo];
         }
+       public bool EsConexo()
+        {
+            if (Ady.Count == 0) return true;
+
+            HashSet<string> visitados = new HashSet<string>();
+            Queue<string> cola = new Queue<string>();
+            string inicio = new List<string>(Ady.Keys)[0];
+
+            cola.Enqueue(inicio);
+            visitados.Add(inicio);
+
+            while (cola.Count > 0)
+            {
+                string actual = cola.Dequeue();
+                foreach (var vecino in Ady[actual])
+                {
+                    if (!visitados.Contains(vecino.destino))
+                    {
+                        visitados.Add(vecino.destino);
+                        cola.Enqueue(vecino.destino);
+                    }
+                }
+            }
+
+            return visitados.Count == Ady.Count;
+        }
+
+        public DijkstraResult Dijkstra(string inicio, string fin)
+        {
+            Dictionary<string, int> dist = new Dictionary<string, int>();
+            Dictionary<string, string> prev = new Dictionary<string, string>();
+            HashSet<string> visitados = new HashSet<string>();
+
+            foreach (var nodo in Ady.Keys)
+            {
+                dist[nodo] = int.MaxValue;
+                prev[nodo] = null;
+            }
+
+            dist[inicio] = 0;
+
+            while (visitados.Count < Ady.Count)
+            {
+                string actual = null;
+                int menor = int.MaxValue;
+
+                foreach (var nodo in Ady.Keys)
+                {
+                    if (!visitados.Contains(nodo) && dist[nodo] < menor)
+                    {
+                        menor = dist[nodo];
+                        actual = nodo;
+                    }
+                }
+
+                if (actual == null) break;
+                visitados.Add(actual);
+
+                foreach (var vecino in Ady[actual])
+                {
+                    int nuevaDist = dist[actual] + vecino.distancia;
+                    if (nuevaDist < dist[vecino.destino])
+                    {
+                        dist[vecino.destino] = nuevaDist;
+                        prev[vecino.destino] = actual;
+                    }
+                }
+            }
+
+            List<string> ruta = new List<string>();
+            string temp = fin;
+
+            while (temp != null)
+            {
+                ruta.Insert(0, temp);
+                temp = prev[temp];
+            }
+
+            return new DijkstraResult(ruta, dist[fin]);
+        }
     }
 }
