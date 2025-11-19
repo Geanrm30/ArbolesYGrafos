70b474a7eafd5e8a6abac0890951bf6e7dc0ab09
Geanrm30 <garodriguezm@uamv.edu.ni>
2025-11-18 20:08:22 -0600

    Arreglando TabIndex

--- ArbolesYGrafos/Form1.Designer.cs
+++ ArbolesYGrafos/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tvArbol = new System.Windows.Forms.TreeView();
             this.label3 = new System.Windows.Forms.Label();
             this.tbNivel = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
@@ -61,7 +62,6 @@
             this.tbOrigen = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.tvArbol = new System.Windows.Forms.TreeView();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -83,9 +83,16 @@
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(412, 547);
-            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Arbol Organizativo";
+            // 
+            // tvArbol
+            // 
+            this.tvArbol.Location = new System.Drawing.Point(9, 223);
+            this.tvArbol.Name = "tvArbol";
+            this.tvArbol.Size = new System.Drawing.Size(403, 324);
+            this.tvArbol.TabIndex = 12;
             // 
             // label3
             // 
@@ -93,7 +100,7 @@
             this.label3.Location = new System.Drawing.Point(6, 169);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 16);
-            this.label3.TabIndex = 10;
+            this.label3.TabIndex = 9;
             this.label3.Text = "Nivel:";
             // 
             // tbNivel
@@ -101,14 +108,14 @@
             this.tbNivel.Location = new System.Drawing.Point(56, 169);
             this.tbNivel.Name = "tbNivel";
             this.tbNivel.Size = new System.Drawing.Size(144, 22);
-            this.tbNivel.TabIndex = 9;
+            this.tbNivel.TabIndex = 10;
             // 
             // btnBuscar
             // 
             this.btnBuscar.Location = new System.Drawing.Point(226, 98);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(141, 27);
-            this.btnBuscar.TabIndex = 8;
+            this.btnBuscar.TabIndex = 7;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
@@ -118,7 +125,7 @@
             this.btnNivel.Location = new System.Drawing.Point(226, 169);
             this.btnNivel.Name = "btnNivel";
             this.btnNivel.Size = new System.Drawing.Size(141, 27);
-            this.btnNivel.TabIndex = 7;
+            this.btnNivel.TabIndex = 11;
             this.btnNivel.Text = "Obtener Nivel";
             this.btnNivel.UseVisualStyleBackColor = true;
             this.btnNivel.Click += new System.EventHandler(this.btnNivel_Click_1);
@@ -128,7 +135,7 @@
             this.btnContar.Location = new System.Drawing.Point(226, 131);
             this.btnContar.Name = "btnContar";
             this.btnContar.Size = new System.Drawing.Size(141, 32);
-            this.btnContar.TabIndex = 6;
+            this.btnContar.TabIndex = 8;
             this.btnContar.Text = "Contar Nodos";
             this.btnContar.UseVisualStyleBackColor = true;
             this.btnContar.Click += new System.EventHandler(this.btnContar_Click_1);
@@ -148,14 +155,14 @@
             this.tbBuscar.Location = new System.Drawing.Point(56, 103);
             this.tbBuscar.Name = "tbBuscar";
             this.tbBuscar.Size = new System.Drawing.Size(144, 22);
-            this.tbBuscar.TabIndex = 4;
+            this.tbBuscar.TabIndex = 6;
             // 
             // tbHijo
             // 
             this.tbHijo.Location = new System.Drawing.Point(56, 64);
             this.tbHijo.Name = "tbHijo";
             this.tbHijo.Size = new System.Drawing.Size(144, 22);
-            this.tbHijo.TabIndex = 3;
+            this.tbHijo.TabIndex = 4;
             // 
             // tbPadre
             // 
@@ -170,7 +177,7 @@
             this.label2.Location = new System.Drawing.Point(6, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 16);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 3;
             this.label2.Text = "Hijo:";
             // 
             // label1
@@ -179,7 +186,7 @@
             this.label1.Location = new System.Drawing.Point(6, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 16);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 1;
             this.label1.Text = "Padre:";
             // 
             // groupBox2
@@ -207,7 +214,7 @@
             this.groupBox2.Location = new System.Drawing.Point(430, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(435, 547);
-            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Grafo";
             // 
@@ -216,7 +223,7 @@
             this.btnAgregarEdificio.Location = new System.Drawing.Point(268, 21);
             this.btnAgregarEdificio.Name = "btnAgregarEdificio";
             this.btnAgregarEdificio.Size = new System.Drawing.Size(157, 31);
-            this.btnAgregarEdificio.TabIndex = 24;
+            this.btnAgregarEdificio.TabIndex = 16;
             this.btnAgregarEdificio.Text = "Agregar Edificio";
             this.btnAgregarEdificio.UseVisualStyleBackColor = true;
             this.btnAgregarEdificio.Click += new System.EventHandler(this.btnAgregarEdificio_Click);
@@ -226,7 +233,7 @@
             this.tbNombreEdificio.Location = new System.Drawing.Point(118, 21);
             this.tbNombreEdificio.Name = "tbNombreEdificio";
             this.tbNombreEdificio.Size = new System.Drawing.Size(144, 22);
-            this.tbNombreEdificio.TabIndex = 23;
+            this.tbNombreEdificio.TabIndex = 15;
             // 
             // label10
             // 
@@ -234,7 +241,7 @@
             this.label10.Location = new System.Drawing.Point(6, 21);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(106, 16);
-            this.label10.TabIndex = 22;
+            this.label10.TabIndex = 14;
             this.label10.Text = "Agregar Edificio:";
             // 
             // tbRuta
@@ -242,7 +249,7 @@
             this.tbRuta.Location = new System.Drawing.Point(291, 141);
             this.tbRuta.Name = "tbRuta";
             this.tbRuta.Size = new System.Drawing.Size(138, 27);
-            this.tbRuta.TabIndex = 21;
+            this.tbRuta.TabIndex = 31;
             this.tbRuta.Text = "Ruta mas corta";
             this.tbRuta.UseVisualStyleBackColor = true;
             this.tbRuta.Click += new System.EventHandler(this.tbRuta_Click);
@@ -252,7 +259,7 @@
             this.btnMostrarConexiones.Location = new System.Drawing.Point(291, 108);
             this.btnMostrarConexiones.Name = "btnMostrarConexiones";
             this.btnMostrarConexiones.Size = new System.Drawing.Size(138, 27);
-            this.btnMostrarConexiones.TabIndex = 20;
+            this.btnMostrarConexiones.TabIndex = 26;
             this.btnMostrarConexiones.Text = "Mostrar Ruta";
             this.btnMostrarConexiones.UseVisualStyleBackColor = true;
             this.btnMostrarConexiones.Click += new System.EventHandler(this.btnMostrarConexiones_Click_1);
@@ -262,7 +269,7 @@
             this.tbValidarConexo.Location = new System.Drawing.Point(291, 174);
             this.tbValidarConexo.Name = "tbValidarConexo";
             this.tbValidarConexo.Size = new System.Drawing.Size(138, 27);
-            this.tbValidarConexo.TabIndex = 19;
+            this.tbValidarConexo.TabIndex = 32;
             this.tbValidarConexo.Text = "Validar Conexo";
             this.tbValidarConexo.UseVisualStyleBackColor = true;
             this.tbValidarConexo.Click += new System.EventHandler(this.tbValidarConexo_Click);
@@ -272,7 +279,7 @@
             this.tbFin.Location = new System.Drawing.Point(173, 174);
             this.tbFin.Name = "tbFin";
             this.tbFin.Size = new System.Drawing.Size(82, 22);
-            this.tbFin.TabIndex = 18;
+            this.tbFin.TabIndex = 30;
             // 
             // label9
             // 
@@ -280,7 +287,7 @@
             this.label9.Location = new System.Drawing.Point(141, 174);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(28, 16);
-            this.label9.TabIndex = 17;
+            this.label9.TabIndex = 29;
             this.label9.Text = "Fin:";
             // 
             // tbInicio
@@ -288,7 +295,7 @@
             this.tbInicio.Location = new System.Drawing.Point(53, 172);
             this.tbInicio.Name = "tbInicio";
             this.tbInicio.Size = new System.Drawing.Size(82, 22);
-            this.tbInicio.TabIndex = 16;
+            this.tbInicio.TabIndex = 28;
             // 
             // label8
             // 
@@ -296,7 +303,7 @@
             this.label8.Location = new System.Drawing.Point(6, 172);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(41, 16);
-            this.label8.TabIndex = 15;
+            this.label8.TabIndex = 27;
             this.label8.Text = "Inicio:";
             // 
             // tbMostrarNodo
@@ -304,7 +311,7 @@
             this.tbMostrarNodo.Location = new System.Drawing.Point(111, 131);
             this.tbMostrarNodo.Name = "tbMostrarNodo";
             this.tbMostrarNodo.Size = new System.Drawing.Size(144, 22);
-            this.tbMostrarNodo.TabIndex = 14;
+            this.tbMostrarNodo.TabIndex = 25;
             // 
             // label7
             // 
@@ -312,40 +319,40 @@
             this.label7.Location = new System.Drawing.Point(6, 131);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(99, 16);
-            this.label7.TabIndex = 13;
+            this.label7.TabIndex = 24;
             this.label7.Text = "Nodo a mostar:";
             // 
             // lsbGrafo
             // 
             this.lsbGrafo.FormattingEnabled = true;
             this.lsbGrafo.ItemHeight = 16;
-            this.lsbGrafo.Location = new System.Drawing.Point(9, 217);
+            this.lsbGrafo.Location = new System.Drawing.Point(13, 223);
             this.lsbGrafo.Name = "lsbGrafo";
             this.lsbGrafo.Size = new System.Drawing.Size(416, 324);
-            this.lsbGrafo.TabIndex = 12;
+            this.lsbGrafo.TabIndex = 33;
             // 
             // tbDistancia
             // 
-            this.tbDistancia.Location = new System.Drawing.Point(372, 86);
+            this.tbDistancia.Location = new System.Drawing.Point(372, 56);
             this.tbDistancia.Name = "tbDistancia";
             this.tbDistancia.Size = new System.Drawing.Size(57, 22);
-            this.tbDistancia.TabIndex = 6;
+            this.tbDistancia.TabIndex = 22;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(226, 89);
+            this.label6.Location = new System.Drawing.Point(235, 59);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(136, 16);
-            this.label6.TabIndex = 5;
+            this.label6.TabIndex = 21;
             this.label6.Text = "Distancia (en metros):";
             // 
             // AgregarConexion
             // 
-            this.AgregarConexion.Location = new System.Drawing.Point(229, 53);
+            this.AgregarConexion.Location = new System.Drawing.Point(238, 78);
             this.AgregarConexion.Name = "AgregarConexion";
-            this.AgregarConexion.Size = new System.Drawing.Size(133, 30);
-            this.AgregarConexion.TabIndex = 4;
+            this.AgregarConexion.Size = new System.Drawing.Size(133, 24);
+            this.AgregarConexion.TabIndex = 23;
             this.AgregarConexion.Text = "Agregar Conexion";
             this.AgregarConexion.UseVisualStyleBackColor = true;
             this.AgregarConexion.Click += new System.EventHandler(this.AgregarConexion_Click);
@@ -355,14 +362,14 @@
             this.tbDestino.Location = new System.Drawing.Point(68, 89);
             this.tbDestino.Name = "tbDestino";
             this.tbDestino.Size = new System.Drawing.Size(144, 22);
-            this.tbDestino.TabIndex = 3;
+            this.tbDestino.TabIndex = 20;
             // 
             // tbOrigen
             // 
             this.tbOrigen.Location = new System.Drawing.Point(68, 53);
             this.tbOrigen.Name = "tbOrigen";
             this.tbOrigen.Size = new System.Drawing.Size(144, 22);
-            this.tbOrigen.TabIndex = 2;
+            this.tbOrigen.TabIndex = 18;
             // 
             // label5
             // 
@@ -370,7 +377,7 @@
             this.label5.Location = new System.Drawing.Point(6, 92);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(56, 16);
-            this.label5.TabIndex = 1;
+            this.label5.TabIndex = 19;
             this.label5.Text = "Destino:";
             // 
             // label4
@@ -379,15 +386,8 @@
             this.label4.Location = new System.Drawing.Point(6, 59);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(50, 16);
-            this.label4.TabIndex = 0;
+            this.label4.TabIndex = 17;
             this.label4.Text = "Origen:";
-            // 
-            // tvArbol
-            // 
-            this.tvArbol.Location = new System.Drawing.Point(9, 217);
-            this.tvArbol.Name = "tvArbol";
-            this.tvArbol.Size = new System.Drawing.Size(403, 324);
-            this.tvArbol.TabIndex = 11;
             // 
             // Form1
             // 
